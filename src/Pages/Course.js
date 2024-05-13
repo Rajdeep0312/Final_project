22d59e16503d6c04609fb9c4c106cda350076df4
Animeshmitra08 <animeshmitra003@gmail.com>
2024-05-14 02:32:13 +0530

    Courses added

--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DrawerAppBar from '../NavComponents/DrawerAppBar'
-import { Box, Toolbar } from '@mui/material'
+import { Box, Container, Grid, Toolbar } from '@mui/material'
 import Footer from '../NavComponents/Footer'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,38 +8,82 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
 
 
 const Course = () => {
+
+    const [courseData, setCourseData] = useState([]);
+
+
+    useEffect(() => {
+        const options = {
+        method: 'GET',
+        url: 'https://courses9.p.rapidapi.com/api/v6/website/labels',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-RapidAPI-Key': '1462331492msh4263adfe366c539p137e91jsn3ce32463ad72',
+            'X-RapidAPI-Host': 'courses9.p.rapidapi.com'
+        }
+        };
+
+        async function getData() {
+            try {
+              const response = await axios.request(options);
+              setCourseData(response.data.data);
+            } catch (error) {
+              console.error(error);
+            }
+          } 
+
+        return() =>{
+            getData()
+        }
+    
+    }, [])
+    
+
+
+
   return (
     <>
         <DrawerAppBar/>
             <Box sx={{p:2}}>
                 <Toolbar/>
 
-                <Typography variant='h3' sx={{}}>Courses</Typography>
+                <Typography variant='h3' sx={{textAlign : 'center', marginBottom : "2rem" , padding : "5px"}}>Courses</Typography>
 
 
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="./images/pic3.jpg"
-                        title="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
+                <Container sx={{mx:'auto', }}>
+                <Grid container spacing={3}>
+                {courseData.map((data)=>(
+
+                <Grid key={data.id} item xs={2} sm={4} md={4} sx={{ padding: "0px"}}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 140 }}
+                            image="./images/pic3.jpg"
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                            {data.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nobis.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View</Button>
+                            <Button size="small">Apply Now</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+
+                ))}
+                </Grid>
+                </Container>
+                
             
             </Box>
         <Footer/>
